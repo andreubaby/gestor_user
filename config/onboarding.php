9e0981f0b7041bc4c05be279f0e9a8d5e7d5adf3
alejandro andreu <soporte@babyplant.es>
2026-01-14 12:55:59 +0100

    onboarding nuevos usuarios

--- config/onboarding.php
+++ config/onboarding.php
@@ -1,1 +1,33 @@
 <?php
+
+return [
+    'packs' => [
+        'basico' => [
+            'name'    => 'Pack básico (obligatorio)',
+            'subject' => 'Babyplant Cursos obligatorios',
+            'intro'   => "Tras haber completado el curso de Riesgos Laborales 🦺✅, recuerda que también debemos realizar los siguientes cursos (muchos de ellos son de cumplimiento legal):\n\n1️⃣ Igualdad de género y prevención del acoso en el trabajo ⚖️\n2️⃣ Prevención del estrés térmico y golpe de calor + primeros auxilios ante calor 🌡️☀️\n3️⃣ Primeros auxilios y actuación ante emergencias 🚑\n4️⃣ Protección de datos, confidencialidad y seguridad informática 🔐💻\n5️⃣ Manipulación manual de cargas 📦👐\n6️⃣ Prevención de delitos penales, ética y conducta profesional 🕵️‍♂️⚖️\n7️⃣ Normas de higiene 🧼🧴\n8️⃣ Medidas de emergencia en Babyplant 🚨🌱\n9️⃣ Concienciación Plan LGTBI 🏳️‍🌈\n\n📲 Para facilitar la realización de estos cursos, hemos preparado un breve vídeo formativo 🎥 para cada uno. Junto con la documentación adicional 📚, explican de forma clara lo que debemos conocer.\n\n🙏🏼 Gracias por vuestra colaboración e implicación.",
+            'items' => [
+                ['type' => 'link', 'title' => 'Plan de igualdad', 'url' => 'https://drive.google.com/file/d/1vGorPW6MZfPgUIyt6MIijjyLJSDU7aFv/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Plan de actuación ante temperaturas ambientales extremas', 'url' => 'https://drive.google.com/file/d/1ubp0XK_nOIAa6N7S0FMfc9GWiON4DtVR/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Procedimientos accidentes emergencias', 'url' => 'https://drive.google.com/file/d/1TKnmOktiZy3UhuS2IJNBaZ4BjVSHSkS_/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Plan de Prevención y Actuación ante el Acoso Sexual', 'url' => 'https://drive.google.com/file/d/1iDoFZWihJcOtmzQZwkGtWr1hNLkaeQi-/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Normas sobre protección de datos', 'url' => 'https://drive.google.com/file/d/18JRLF4N85RFH_Yli1au2lwEjY6h-jrci/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Normas higiene personal', 'url' => 'https://drive.google.com/file/d/1M910czGJS8H7iiG-4-FZFnnrhpDCHUpN/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Normas de seguridad', 'url' => 'https://drive.google.com/file/d/1k6Gz4PzoEkAe40ru_SCdJ92lf2G1k6Sl/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Método de empleo de un extintor', 'url' => 'https://drive.google.com/file/d/1FYpGv1alluHBJfyMt7lDf98qDjNy_y5R/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Actuación en caso de incendio y evacuación', 'url' => 'https://drive.google.com/file/d/1dWW7fHq_-CLR1cEXVqoqcHxgs2q8ukO9/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Cartel no al acoso sexual en el trabajo', 'url' => 'https://drive.google.com/file/d/1P2O1hYu2fyjda-28Jz-TJyw_v7hWTG_G/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Carta Plan de prevención de delitos', 'url' => 'https://drive.google.com/file/d/1wT44W8ESAKCGBoOq1Xl_LWNKbjEBqqKs/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Curso sobre Igualdad de género y prevención del acoso en el Trabajo', 'url' => 'https://drive.google.com/file/d/1ddoHc--WFyn7iYB83-uwyDpt-QxqYdTc/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Prevención del Estrés Térmico y Golpe de Calor y Primeros Auxilios ante Calor', 'url' => 'https://drive.google.com/file/d/1N8J5ZppyT847PFhTfGsVhCcbu7Zzb-oL/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Primeros Auxilios y Actuación ante Emergencias', 'url' => 'https://drive.google.com/file/d/1XaLtmYXm5MQsu2npuVkKQrupAQkioIkj/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Protección de Datos, Confidencialidad y Seguridad Informática', 'url' => 'https://drive.google.com/file/d/1Hsdy8fACZ4blJbaOeY0ZB5N4yW2hNdpi/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Manipulación Manual de Cargas', 'url' => 'https://drive.google.com/file/d/1sLHFncUCnF2u8Ik-FnKBNg7DQXgIcAfA/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Prevención de Delitos Penales, Ética y Conducta Profesional', 'url' => 'https://drive.google.com/file/d/1Odx96kAXyYRGA1z8Oh1z9zMs1tyB4Z0W/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Normas de Higiene', 'url' => 'https://drive.google.com/file/d/1xrZ_C1fI_LpSlOGWYgfTrUwBrjqvqvdX/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Medidas de Emergencia en Babyplant', 'url' => 'https://drive.google.com/file/d/1Oq0JPguxc2LbDj3COz3LNGmqGUvvIdkZ/view?usp=drive_link'],
+                ['type' => 'link', 'title' => 'Concienciación Plan LGTBI', 'url' => 'https://drive.google.com/file/d/1uhd6AnD9ILbLmjZ00xkf0W3aHo_Oq97D/view?usp=drive_link'],
+            ],
+        ],
+    ],
+];
